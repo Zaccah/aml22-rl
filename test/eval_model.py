d86000f294ff665e4e473f24a7b801833ab2d5ae
Zaccah <zaccarialuca86@gmail.com>
2023-01-09 18:19:46 +0100

    added UDR trained model and log

--- test/eval_model.py
+++ test/eval_model.py
@@ -5,7 +5,7 @@ from stable_baselines3 import PPO
 
 def main():
     render = False
-    test_env = 'CustomHopper-source-v0'
+    test_env = 'CustomHopper-target-v0'
 
     env = gym.make(test_env)
     try:
@@ -13,7 +13,7 @@ def main():
             "lr_schedule": .0003,
             "clip_range": .2
         }
-        model = PPO.load(r'trained_models/PPO_standard_source.zip', env=env, custom_objects=custom_objects)
+        model = PPO.load(r'trained_models/PPO_standard_target.zip', env=env, custom_objects=custom_objects)
     
     except IOError:
         print("File not found, check the relative path and run in a terminal within the folder")
